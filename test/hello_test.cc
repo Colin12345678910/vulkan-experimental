3a7c2e79a0e04fda0480039628b7a1fc9512e734
Colin12345678910 <84333388+Colin12345678910@users.noreply.github.com>
2025-08-13 16:16:00 -0400

    Setup the project structure to properly support testing.

---
+++ test/hello_test.cc
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include <vk_engine.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(HelloTest, CameraExample)
+{
+	Camera c;
+	EXPECT_STRNE("fail", "fail");
+	//c.velocity = glm::vec3(1 * 0.15, 0, 0);
+	//c.Update();
+
+	//EXPECT_EQ(c.position, glm::vec3(1, 0, 0));
+}
+
+/*
+* Honestly, I'm not sure if testing engine init is paticularly "Good practice"
+* but it does verify that this entire testing framework can link against the engine properly.
+* If the engine fails to initialize, well, that's a problem.
+*/
+TEST(HelloTest, EngineInvoke)
+{
+	VulkanEngine engine;
+	engine.init();
+
+	engine.cleanup();
+}
